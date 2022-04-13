b6b4cf1395693edaa5c832992a3611d1b1696563
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-04-12 21:55:48 -0300

    first commit

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Heyy, I am a contract, I am smart, and I am developed by David :D");
+    }
+}

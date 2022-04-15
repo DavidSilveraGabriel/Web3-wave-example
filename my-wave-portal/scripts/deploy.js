e6e5e4546c94ca4c2fc65c3d0450206668212881
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2022-04-15 11:14:39 -0300

    update

--- my-wave-portal/scripts/deploy.js
+++ my-wave-portal/scripts/deploy.js
@@ -1,10 +1,10 @@
 const main = async () => {
     const [deployer] = await hre.ethers.getSigners();
     const accountBalance = await deployer.getBalance();
-  
+    // print address and balance of deployer
     console.log("Deploying contracts with account: ", deployer.address);
     console.log("Account balance: ", accountBalance.toString());
-  
+    // wave contracts
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
